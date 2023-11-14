514cb8734aa61a411e1d0e1e158dfa53fe3bc6e4
Ssssv11 <56876234+Ssssv11@users.noreply.github.com>
2023-11-14 18:38:23 +0800

    edit title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "狗子GPT",
+  title: "狗子 GPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "狗子GPT",
+    title: "狗子 GPT",
     statusBarStyle: "default",
   },
 };
